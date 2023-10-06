ae123e8084ed98f0ce692abe1a282064ccd9825d
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-10-06 18:47:09 +0200

    Initial commit

---
+++ Practica1/Practica1_2.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity sum1 is
+	port(A, B, EA : in std_logic;
+	port S, SC : out std_logic);
+end sum1;
+
+architecture Arch_sum1 of sum1 is
+begin
+	S <= (EA XOR (A XOR B));
+	SC <= ((A AND B) OR (EA AND (A XOR B)));
+end Arch_sum1;
