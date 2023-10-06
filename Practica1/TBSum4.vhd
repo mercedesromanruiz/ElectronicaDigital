29898760b18428145a284ca758890457cdf0b257
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-10-06 20:15:40 +0200

    Práctica 1.2 Completada Bien

--- Practica1/TBSum4.vhd
+++ Practica1/TBSum4.vhd
@@ -1,15 +1,36 @@
+library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity TBSum4 is
 end TBSum4;
 
 architecture Arch_TBSum4 of TBSum4 is
-	signal TB_X, TB_Y, TB_S : std_logic_vector;
-	signal TB_Ci : std_logic;
+	signal TB_X, TB_Y, TB_S : std_logic_vector (3 downto 0);
+	signal TB_Ci, TB_Co : std_logic;
 begin
-	sum4: entity work.sum4 port map ();
+	sum4: entity work.sum4 port map (TB_X, TB_Y, TB_S, TB_Co, TB_Ci);
 
-	TB_X <= "1111", "0000" after 40ns;
-	TB_Y <= "1111", "0000" after 20ns, "1111" after 40ns, "0000" after 60ns;
-	TB_Ci <= '0', '1' after 10ns, '0' after 20ns, '1' after 30ns, '0' after 40ns, '1' after 50ns, '0' after 60ns, '1' after 60ns, '0' after 70ns;
+	process
+	begin
+		TB_X <= "1111";
+		TB_Y <= "1111";
+		TB_Ci <= '0';
+		wait for 10 ns;
+	
+		TB_X <= "1111";
+		TB_Y <= "1111";
+		TB_Ci <= '1';
+		wait for 10 ns;
+
+		TB_X <= "0000";
+		TB_Y <= "0000";
+		TB_Ci <= '0';
+		wait for 10 ns;
+
+		TB_X <= "1111";
+		TB_Y <= "1111";
+		TB_Ci <= '1';
+		wait for 10 ns;	
+	end process;
+
 end Arch_TBSum4;
